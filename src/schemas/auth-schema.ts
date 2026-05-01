05231cc83697ce3a087ed9caec243c662ad9d6ff
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-05-01 20:43:02 +0800

    feat(auth-schema): enhance validation messages for login and registration schemas

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -2,11 +2,14 @@ import z from "zod";
 
 export const LogInSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(1),
+  password: z.string().min(1, "Password is required"),
 });
 
 export const registerSchema = z.object({
-  name: z.string().min(6).max(32),
+  name: z
+    .string()
+    .min(6, "Name must be at least 6 characters long")
+    .max(32, "Name must be at most 32 characters long"),
   email: z.string().email(),
-  password: z.string().min(6),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
 });

bd05c2b758a9c2508eb1f17753e32022bdaf52a8
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-05-01 20:04:19 +0800

    feat(auth): enhance authentication page with tabs and button group
    - Added a new ButtonGroup component for better button layout.
    - Implemented tabs for Sign In and Sign Up forms in the authentication page.
    - Included social authentication options with Google and GitHub.
    - Updated the UI with a header and description for the ICTQuest platform.
    - Added a prototype image to the assets.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,6 +3,7 @@ import { AuthProviderButton } from "@/components/auth-provider-button";
 import { LogInForm } from "@/components/login-form";
 import { SignupForm } from "@/components/sign-up-form";
 import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
 import {
   Card,
   CardContent,
@@ -17,6 +18,61 @@ export default function AuthPage() {
   return (
     <div className="flex min-h-[80vh] items-center justify-center max-sm:px-4">
       <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="font-bold text-4xl">ICTQuest</CardTitle>
+          <CardDescription className="text-muted-foreground">
+            Your ultimate platform for ICT learning and growth
+          </CardDescription>
+          <CardContent className="mt-6 px-0">
+            <Tabs className="w-full" defaultValue="signin">
+              <TabsList asChild className="mb-4 grid w-full grid-cols-2">
+                <ButtonGroup>
+                  <TabsTrigger className="cursor-pointer" value="signin">
+                    Sign In
+                  </TabsTrigger>
+                  <TabsTrigger className="cursor-pointer" value="signup">
+                    Sign Up
+                  </TabsTrigger>
+                </ButtonGroup>
+              </TabsList>
+              <TabsContent value="signin">
+                <LogInForm />
+                <Button className="mt-2 cursor-pointer p-0" variant="link">
+                  Forgot password?
+                </Button>
+              </TabsContent>
+              <TabsContent value="signup">
+                <SignupForm />
+                <br />
+              </TabsContent>
+            </Tabs>
+
+            <div className="mt-6 flex flex-col gap-10">
+              <div className="relative">
+                <Separator className="absolute top-[50%] bottom-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-gray-400" />
+                <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-card px-4 text-gray-800 dark:text-gray-400">
+                  OR CONTINUE WITH
+                </span>
+              </div>
+              <div className="space-y-2">
+                <ButtonGroup className="w-full gap-2" orientation={"vertical"}>
+                  <AuthProviderButton
+                    icon={<FaGoogle />}
+                    label="Continue with Google"
+                    provider="google"
+                  />
+                  <AuthProviderButton
+                    icon={<FaGithub />}
+                    label="Continue with Github"
+                    provider="github"
+                  />
+                </ButtonGroup>
+              </div>
+            </div>
+          </CardContent>
+        </CardHeader>
+      </Card>
+      {/* <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle className="font-bold text-2xl">
             Welcome to ICTQuest
@@ -68,7 +124,7 @@ export default function AuthPage() {
             </div>
           </div>
         </CardContent>
-      </Card>
+      </Card> */}
     </div>
   );
 }

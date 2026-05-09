db3841544d5165be40d1bda935ad8b8416aba5c4
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-05-09 20:35:33 +0800

    ref: clean up AuthPage component by removing commented-out code and ensuring consistent styling

--- src/app/(public)/auth/page.tsx
+++ src/app/(public)/auth/page.tsx
@@ -50,7 +50,7 @@ export default function AuthPage() {
             <div className="mt-6 flex flex-col gap-10">
               <div className="relative">
                 <Separator className="absolute top-[50%] bottom-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-gray-400" />
-                <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-card px-4 text-gray-800 dark:text-gray-400">
+                <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-nowrap bg-card px-4 text-gray-800 dark:text-gray-400">
                   OR CONTINUE WITH
                 </span>
               </div>
@@ -72,59 +72,6 @@ export default function AuthPage() {
           </CardContent>
         </CardHeader>
       </Card>
-      {/* <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle className="font-bold text-2xl">
-            Welcome to ICTQuest
-          </CardTitle>
-          <CardDescription className="text-muted-foreground">
-            Sign in to your account or create a new one
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="mt-6">
-          <Tabs className="w-full" defaultValue="signin">
-            <TabsList className="mb-4 grid w-full grid-cols-2">
-              <TabsTrigger className="cursor-pointer" value="signin">
-                Sign In
-              </TabsTrigger>
-              <TabsTrigger className="cursor-pointer" value="signup">
-                Sign Up
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="signin">
-              <LogInForm />
-              <Button className="mt-2 cursor-pointer p-0" variant="link">
-                Forgot password?
-              </Button>
-            </TabsContent>
-            <TabsContent value="signup">
-              <SignupForm />
-              <br />
-            </TabsContent>
-          </Tabs>
-
-          <div className="mt-6 flex flex-col gap-10">
-            <div className="relative">
-              <Separator className="absolute top-[50%] bottom-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-gray-400" />
-              <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-card px-4 text-gray-800 dark:text-gray-400">
-                OR CONTINUE WITH
-              </span>
-            </div>
-            <div className="space-y-2">
-              <AuthProviderButton
-                icon={<FaGoogle />}
-                label="Continue with Google"
-                provider="google"
-              />
-              <AuthProviderButton
-                icon={<FaGithub />}
-                label="Continue with Github"
-                provider="github"
-              />
-            </div>
-          </div>
-        </CardContent>
-      </Card> */}
     </div>
   );
 }

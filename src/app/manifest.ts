bfa7a31d40d80da50432e6476dd8d1cda4b5fd2e
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-06-22 10:52:25 +0800

    chore: Add manifest file for Quizlet PWA configuration

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Quizlet | Create and Share Flashcards with Ease",
+    short_name: "Quizlet",
+    description:
+      "Create and share flashcards with ease using Quizlet, the ultimate study tool.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#2d3639",
+    theme_color: "#f0f0f0",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}

bdaa643e42b64766b616368d1f0f065b93d75412
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-06-22 18:05:17 +0800

    feat: enhance header layout and add titles for flashcard set and test pages

--- src/app/(app)/flashcardset/[id]/page.tsx
+++ src/app/(app)/flashcardset/[id]/page.tsx
@@ -50,15 +50,20 @@ export default function FlashcardSetPage({
         <AppSidebar />
         <SidebarInset className="min-w-0">
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="flex w-full items-center justify-between px-4 md:justify-start">
+            <div className="flex w-full items-center justify-between px-4 md:justify-start md:gap-4">
+              <SidebarTrigger className="-ml-1 max-md:hidden" />
               <div className="gap2 flex items-center">
-                <SidebarTrigger className="-ml-1" />
+                <SidebarTrigger className="-ml-1 md:hidden" />
+                <MagicBackButton backLink={"/"} className="max-md:hidden" />
                 <Separator
                   className="mr-2 data-vertical:h-4 data-vertical:self-auto"
                   orientation="vertical"
                 />
+                <h1 className="font-semibold text-lg tracking-tight">
+                  Flashcard Set
+                </h1>
               </div>
-              <MagicBackButton />
+              <MagicBackButton backLink={"/"} className="md:hidden" />
             </div>
           </header>
           <main className="mx-auto flex w-full min-w-0 max-w-6xl justify-center py-4">

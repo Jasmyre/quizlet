edda69ed6c2813c48731e166e09bd80b24453a3a
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-06-28 16:36:19 +0800

    feat: update manifest settings for fullscreen display and dark theme

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,9 +7,9 @@ export default function manifest(): MetadataRoute.Manifest {
     description:
       "Create and share flashcards with ease using Quizlet, the ultimate study tool.",
     start_url: "/",
-    display: "standalone",
-    background_color: "#f0f0f0",
-    theme_color: "#2d3639",
+    display: "fullscreen",
+    background_color: "#121212",
+    theme_color: "##121212",
     icons: [
       {
         src: "/icon-192x192.png",
